ab7330a1d6d7469eaf7a5e57d4731a3fa602fad6
Xanthus <Xanthus58@protonmail.com>
2022-02-16 19:42:44 +1030

    1.0
    <3

---
+++ Run Me First.bat
@@ -0,0 +1,5 @@
+REM installs all of the files needed for the afformations program
+winget install 9PJPW5LDXLZ5
+pip install --upgrade pip
+pip install requests
+pip install keyboard
---
+++ uninstaller.bat
@@ -0,0 +1,3 @@
+REM Uninstalls all of the files needed for the afformations program
+pip uninstall pip
+winget uninstall --name "python 3.10"
