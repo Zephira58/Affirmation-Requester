488ba27f535bb9d6cce8d5c54c672b4e53ac90f2
Xanthus <Xanthus58@protonmail.com>
2022-02-24 19:14:43 +1030

    Added an EXE

--- Run Me First.bat
+++
@@ -1,6 +0,0 @@
-REM installs all of the files needed for the affirmations program
-winget install 9PJPW5LDXLZ5
-pip install --upgrade pip
-pip install requests
-pip install keyboard
-pip install colorama
